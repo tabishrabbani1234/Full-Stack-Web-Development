7e4ac1e8a46a1a68d63516f8fce2cedcaa403b5d
tabishrabbani1234 <47188499+tabishrabbani1234@users.noreply.github.com>
2023-03-26 00:28:28 +0530

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -2,7 +2,7 @@ package com.morphleLabs.machine.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.morphleLabs.machine.domain.MachineConstants as machineConstants;
+import com.morphleLabs.machine.domain.MachineConstants;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,53 +10,53 @@ import javax.servlet.http.HttpServletResponse;
 public class Machine {
     int row_pos;
     int col_pos;
-    public static List<String> eventList = new ArrayList<String>();;
+    public static List<String> eventList = new ArrayList<String>();
     public static long eventTime = 0;
     public static String state = "idle";
-    Machine(){
-        this.row_pos = machineConstants.midRowIndex;
-        this.col_pos = machineConstants.midColIndex;
-        state = machineConstants.focus;
+    public Machine(){
+        this.row_pos = MachineConstants.midRowIndex;
+        this.col_pos = MachineConstants.midColIndex;
+        state = MachineConstants.focus;
     }
 
-    public static void updateEventList(String event, long timeStamp){
+    public final void updateEventList(String event, long timeStamp){
         if(timeStamp < eventTime){
             eventList.add(event);
         } else{
-            this.updateMachineState();
+            this.updateMachineState(timeStamp);
             this.updateMachinePosition();
             this.updateTimeStamp(timeStamp);
             eventList.clear();
         }
     }
-    public final updateMachinePosition(){
+    public final void updateMachinePosition(){
         for(int i=0;i<eventList.size();i++){
-            if(eventList[i] == machineConstants.leftArrow){
+            if(eventList.get(i).equals(MachineConstants.leftArrow)){
                 this.col_pos -= 1;
-            } else if(eventList[i] == machineConstants.rightArrow){
+            } else if(eventList.get(i).equals(MachineConstants.rightArrow)){
                 this.col_pos += 1;
-            } else if(eventList[i] == machineConstants.upArrow){
+            } else if(eventList.get(i).equals(MachineConstants.upArrow)){
                 this.row_pos -= 1;
-            } else if(eventList[i] == machineConstants.upArrow){
+            } else if(eventList.get(i).equals(MachineConstants.downArrow)){
                 this.row_pos += 1;
             }
         }
     }
-    public static void updateTimeStamp(long timeStamp){
-        if(state == machineConstants.focus){
+    public final void updateTimeStamp(long timeStamp){
+        if(state.equals(MachineConstants.focus)){
             eventTime = timeStamp + 3000;
-        } else if(state == machineConstants.capture){
+        } else if(state.equals(MachineConstants.capture)){
             eventTime = timeStamp + 2000;
         }
     }
-    public static void updateMachineState(long timeStamp){
+    public final void updateMachineState(long timeStamp){
         if(eventList.size() == 0){
-            if(timeStamp == this.eventTime && state = machineConstants.focus){
-                state = machineConstant.capture;
-            } else if(state == machineConstants.capture){
-                state = machineConstants.idle;
+            if(timeStamp == this.eventTime && state == MachineConstants.focus){
+                state = MachineConstants.capture;
+            } else if(state.equals(MachineConstants.capture)){
+                state = MachineConstants.idle;
             } else {
-                state = machineConstants.focus;
+                state = MachineConstants.focus;
             }
         }
     }
