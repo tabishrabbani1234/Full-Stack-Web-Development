b12dc2f2e6750050ffe87816ee936a5d15f95dc8
tabishrabbani1234 <47188499+tabishrabbani1234@users.noreply.github.com>
2023-03-26 00:29:10 +0530

    Update MachineConstants.java

--- MachineConstants.java
+++ MachineConstants.java
@@ -1,15 +1,15 @@
 package com.morphleLabs.machine.domain;
-
+import java.lang.String;
 public class MachineConstants {
-    public const int maxRowIndex = 20;
-    public const int maxColIndex = 60;
-    public const int midRowIndex = 10;
-    public const int midColIndex = 30;
-    public const String idle = "Idle";
-    public const String focus = "Focusing";
-    public const String capture = "Capturing";
-    public const String leftArrow = "Move Left";
-    public const String rightArrow = "Move Right";
-    public const String upArrow = "Move Up";
-    public const String downArrow = "Move Down";
+    public static final int maxRowIndex = 20;
+    public static final int maxColIndex = 60;
+    public static final int midRowIndex = 10;
+    public static final int midColIndex = 30;
+    public static final String idle = "Idle";
+    public static final String focus = "Focusing";
+    public static final String capture = "Capturing";
+    public static final String leftArrow = "Move Left";
+    public static final String rightArrow = "Move Right";
+    public static final String upArrow = "Move Up";
+    public static final String downArrow = "Move Down";
 }
