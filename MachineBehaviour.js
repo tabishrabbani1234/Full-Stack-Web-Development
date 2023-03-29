8348ec9564f0eb1f516dedefc09970e40da6c03f
tabishrabbani1234 <47188499+tabishrabbani1234@users.noreply.github.com>
2023-03-30 01:54:04 +0530

    Update MachineBehaviour.js

--- MachineBehaviour.js
+++ MachineBehaviour.js
@@ -1,14 +1,11 @@
 <script>
-var eventType = "Initialized";
+var eventType;
 var machineState;
 var eventTimeStamp;
-
-//Created machine class. It can be used to scale the code to multiple machines with few changes.
 class Machine {
    var timeStamp;
    var row_Index;
    var col_Index;
-   var machineStateTimeStamp;
    Machine(){
       timeStamp = 0;
       row_Index = 10;
@@ -20,57 +17,37 @@ class Machine {
       this.col_Index = col;
    }
 }
-Machine machine;
-
-//Created event handler function to update eventType on key press from user
-function createEventHandlerContainer(event){
-     if (event.defaultPrevented) {
-       return ;
-     }
-     eventTimeStamp = event.timeStamp;
-     switch (event.key) {
-       case "ArrowDown":
-         eventType = "Move Down";
-         break;
-       case "ArrowUp":
-         eventType = "Move Up";
-         break;
-       case "ArrowLeft":
-         eventType = "Move Left";
-         break;
-       case "ArrowRight":
-         eventType = "Move Right";
-         break;
-       default:
-         alert("Wrong button entered");
-         return;
-     }
-     event.preventDefault();
-     removeGridElementStyle();
-     addGridElementStyle();
-     requestHandlerFunction();
+var machine = new Machine();
+function upArrowClick(){
+   eventType = "Move Up";
+   removeGridElementStyle();
+   addGridElementStyle();
+   requestHandlerFunction();
 }
-//Update machine state based on backend response
-function responseHandlerFunction(var timeStamp, var state, var x_pos, var y_pos){
-     if(timeStamp !== machine.machineStateTimeStamp){
-         removeGridElementStyle();
-         machine.updateMachine(timeStamp,x_pos,y_pos);
-         addGridElementStyle();
-         machineState = state;
-         setMachineRequestcall(machine.timeStamp);
-     }
+function leftArrowClick(){
+   eventType = "Move Left";
+   removeGridElementStyle();
+   addGridElementStyle();
+   requestHandlerFunction();
+}
+function rightArrowClick(){
+   eventType = "Move Right";
+   removeGridElementStyle();
+   addGridElementStyle();
+   requestHandlerFunction();
+}
+function downArrowClick(){
+   eventType = "Move Down";
+   removeGridElementStyle();
+   addGridElementStyle();
+   requestHandlerFunction();
+}
+function getTimeStamp(event) {
+  eventTimeStamp = Math.floor(event.timeStamp);
 }
 
-//When backend returns the next time, it should be called. Setting up timeout to call backend again
-function setMachineRequestcall(var time){
-   var currentTime = new Date();
-   var timeInMilliSeconds = time - currentTime.getMilliseconds();
-   window.setTimeout(requestHandlerFunction, timeInMilliSeconds);
-}
-
-//Add css property to current machine position
 function addGridElementStyle() {
-    var count = (machine.row_Index * 60) + machine.col_Index ;
+    var count = ((machine.row_Index-1) * 60) + machine.col_Index ;
     var cell = ("div").concat(count.toString());
     var div = document.getElementById(cell);
     div.focus();
@@ -84,29 +61,25 @@ function addGridElementStyle() {
     }
 }
 
-//When machine current position changes, update the last grid css property
 function removeGridElementStyle() {
-    var count = (machine.row_Index * 60) + machine.col_Index ;
+    var count = ((machine.row_Index-1) * 60) + machine.col_Index ;
     var cell = ("div").concat(count.toString());
     var div = document.getElementById(cell);
     div.style.background = "white";
     div.style.border-style = "none";
 }
 
-//Dynamically create grid of 20*60 and initialize the machine.
 function createGrid(){
    for(var i = 0; i < 20; i++) {
       var row = document.createElement("div");
        for(var j = 0; j< 60; j++) {
            count = i*60 + j + 1;
            var cell = ("div").concat(count.toString());
-           var div = document.createElement(cell);
-           div.setAttribute("type", "text");
-           div.setAttribute("method", "post");
+           var div = document.createElement("div");
+           div.setAttribute("id", cell);
            div.style.width = "15px";
            div.style.height = "25px";
            div.style.background = "white";
-           div.addEventListener("keydown", createEventHandlerContainer, true);
            div.style.display = "inline-block";
            row.appendChild(div);
        }
@@ -116,17 +89,17 @@ function createGrid(){
      elem.parentNode.removeChild(elem);
      elem = document.getElementById('Machine Header');
      elem.parentNode.removeChild(elem);
-     machine = new Machine();
+     eventType = "Initialized";
+     eventTimeStamp = 0;
      addGridElementStyle();
      requestHandlerFunction()
 }
 
-//Send request with event type and event timestamp
 function requestHandlerFunction(){
       const xhr = new XMLHttpRequest();
       var url = ("https://localhost:8080/container/").concat(toString((machine.rowIndex - 1)*60 + machine.colIndex);
       xhr.open("POST", url, true);
-      xhr.setRequestHeader('timeStamp', machine.timeStamp);
+      xhr.setRequestHeader('timeStamp', eventTimeStamp);
       xhr.setRequestHeader('eventType', eventType);
       xhr.send();
       xhr.onload = () => {
@@ -140,6 +113,22 @@ function requestHandlerFunction(){
           console.log(`Error: ${xhr.status}`);
         }
       };
+}
+//Response Handler functions
+function responseHandlerFunction(var timeStamp, var state, var x_pos, var y_pos){
+     if(timeStamp !== machine.timeStamp){
+         removeGridElementStyle();
+         machine.updateMachine(timeStamp,x_pos,y_pos);
+         addGridElementStyle();
+         machineState = state;
+         setMachineRequestcall(machine.timeStamp);
+     }
+}
+
+function setMachineRequestcall(var time){
+   var currentTime = new Date();
+   var timeInMilliSeconds = time - currentTime.getMilliseconds();
+   window.setTimeout(requestHandlerFunction, timeInMilliSeconds);
 }
 
 </script>
