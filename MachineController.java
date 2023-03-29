55b41daf6ffe4324ecc9a77ab3800dba08696b53
tabishrabbani1234 <47188499+tabishrabbani1234@users.noreply.github.com>
2023-03-30 03:33:52 +0530

    Update MachineController.java

--- MachineController.java
+++ MachineController.java
@@ -3,6 +3,7 @@ package com.morphleLabs.machine.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.morphleLabs.machine.domain.Machine;
@@ -12,14 +13,16 @@ public class MachineController {
     Machine machine;
     public static boolean initialized = false;
     @RequestMapping("/")
+    @ResponseBody
     public HttpServletResponse helloMessage(HttpServletRequest request, HttpServletResponse response) {
         long timeStamp = request.getHeader("timeStamp");
-        String event = request.getHeader("event");
+        String event = request.getHeader("eventType");
         if(!initialized){
             machine = new Machine();
             initialized = true;
         }
         machine.updateEventList(event,timeStamp);
-        return machine.responseBuilder(response);
+        response.setStatus(200);
+        return "Body";
     }
 }
