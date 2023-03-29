8e9cdfe88db3386bec74f12137c7646a699afa66
tabishrabbani1234 <47188499+tabishrabbani1234@users.noreply.github.com>
2023-03-30 01:56:02 +0530

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -2,17 +2,16 @@ package com.morphleLabs.machine.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.morphleLabs.machine.domain.MachineConstants;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Entity
 public class Machine {
-    int row_pos;
-    int col_pos;
+    public int row_pos;
+    public int col_pos;
     public static List<String> eventList = new ArrayList<String>();
     public static long eventTime = 0;
-    public static String state = "idle";
+    public static String state = "Idle";
     public Machine(){
         this.row_pos = MachineConstants.midRowIndex;
         this.col_pos = MachineConstants.midColIndex;
@@ -51,16 +50,18 @@ public class Machine {
     }
     public final void updateMachineState(long timeStamp){
         if(eventList.size() == 0){
-            if(timeStamp == this.eventTime && state == MachineConstants.focus){
+            if(timeStamp == eventTime && state.equals(MachineConstants.focus)){
                 state = MachineConstants.capture;
             } else if(state.equals(MachineConstants.capture)){
                 state = MachineConstants.idle;
             } else {
                 state = MachineConstants.focus;
             }
+        } else {
+            state = MachineConstants.focus;
         }
     }
-    public final HttpServletResponse responseBuilder() {
+    public final HttpServletResponse responseBuilder(HttpServletResponse response) {
        response.setHeader("machineState" , state);
        response.setHeader("eventTime" , eventTime);
        response.setHeader("rowIndex" , this.row_pos);
